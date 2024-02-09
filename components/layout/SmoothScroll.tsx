b9cfc3ab0f9f6c6650226abfbd550cf56c1e83d9
Stephanie Li <stephanieli@stephanies-air.home>
2024-02-09 13:21:54 -0500

    fixed deployment errors

--- components/layout/SmoothScroll.tsx
+++ components/layout/SmoothScroll.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ReactLenis } from "@studio-freight/react-lenis";
 
-export default function SmoothScroller({ children }) {
+export default function SmoothScroller({ children }: any) {
   return <ReactLenis root>{children}</ReactLenis>;
 }
